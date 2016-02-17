3d0ce3ee351bbb4d242c9d00a011a125a70dd410
Julian V. Modesto <julianvmodesto@gmail.com>
2016-02-17 02:18:28 -0500

    Edit 404

--- src/views/NotFoundView/NotFoundView.js
+++ src/views/NotFoundView/NotFoundView.js
@@ -5,9 +5,9 @@ export class NotFoundView extends React.Component {
   render () {
     return (
       <div className='container text-center'>
-        <h1>This is a demo 404 page!</h1>
+        <h1>404 Not Found</h1>
         <hr />
-        <Link to='/'>Back To Home View</Link>
+        <Link to='/'>Back To Home</Link>
       </div>
     )
   }
